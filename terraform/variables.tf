fa82bb8062e04d469ed930262c10765b45ad49df
BalagouniNaveen <162671532+BalagouniNaveen@users.noreply.github.com>
2025-07-26 17:16:03 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "image_tag" {
+  type        = string
+  description = "Docker image tag to deploy"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "Your AWS account ID (for ECR images)"
+}
